249658a5dff11f6b853cceb88c1e667182c27766
hurlatunde <hurlatunde@gmail.com>
2016-09-21 12:24:10 +0100

    clean up by removing unwanted code

--- app/component/js/view/layout.js
+++ app/component/js/view/layout.js
@@ -34,29 +34,13 @@ function layoutUrl(p) {
  * @thirdParams    (Optional) "Data"- data to be sent to the layout
  */
 function includeElement(container, htmlSource, data) {
-    // if (data == undefined) {
-    //     data = {};
-    // }
-    // $.get('/layouts/elements/'+htmlSource+'.html', function (template) {
-    //     var rendered = Mustache.render(template, data);
-    //     layoutUrl({element: container, htmlSource: rendered, renderedHTML: true});
-    // }).error(function(jqXHR, textStatus, errorThrown) {
-    //     if (textStatus == 'error' && errorThrown == 'Not Found'){
-    //         data.error_message = "Element file not found ** /layouts/elements/"+htmlSource+".html **";
-    //         data.error_layout = htmlSource;
-    //         data.not_found = false;
-    //         $.get(CONFIG.layoutTemplate('404'), function (template) {
-    //             var rendered = Mustache.render(template, data);
-    //             layoutUrl({element: container, htmlSource: rendered, renderedHTML: true});
-    //         });
-    //         return;
-    //     }
-    // });
     container = $('#'+container);
     parseTemplate(container,"elements/"+htmlSource+".html",data);
 }
 
-
+/**
+ *
+ */
 function parseTemplate(container,htmlSource,data){
     if (data == undefined) {
         data = {};
@@ -115,27 +99,8 @@ function renderLayout(layout, container, data) {
     }
 
     parseTemplate(container,CONFIG.layoutTemplate(layout));
-
-    // $.get(CONFIG.layoutTemplate(layout), function (template) {
-    //
-    //     var rendered = Mustache.render(template, data);
-    //     layoutUrl({element: container, htmlSource: rendered, renderedHTML: true});
-    //
-    // }).error(function(jqXHR, textStatus, errorThrown) {
-    //     if (textStatus == 'error' && errorThrown == 'Not Found'){
-    //         data.error_message = "File not found ** /layouts/"+layout+".html **";
-    //         data.error_layout = layout;
-    //         data.not_found = false;
-    //         $.get(CONFIG.layoutTemplate('404'), function (template) {
-    //             var rendered = Mustache.render(template, data);
-    //             layoutUrl({element: container, htmlSource: rendered, renderedHTML: true});
-    //         });
-    //         return;
-    //     }
-    // });
 }
 
 if (CONFIG.private('loading') == true) {
-    //renderLayout('page_loading', swallowJsContainer);
     layoutUrl({element: swallowJsContainer, htmlSource: CONFIG.layoutTemplate('page_loading')});
 }
