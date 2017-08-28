267e06d5cec788bb17c1370c41103210e84e3daa
hurlatunde <hurlatunde@gmail.com>
2017-08-28 19:47:11 +0100

    removed unwanted code from the pull

--- app/routes.js
+++ app/routes.js
@@ -33,19 +33,20 @@ var default_container = $('#default_container');
 Path.map("#/").to(function () {
 
     //An example of an array
-    var data = {
-        "beatles": [
-            {"firstName": "John", "lastName": "Lennon"},
-            {"firstName": "Paul", "lastName": "McCartney"},
-            {"firstName": "George", "lastName": "Harrison"},
-            {"firstName": "Ringo", "lastName": "Starr"}
-        ],
-        "name": function () {
-            return this.firstName + " " + this.lastName;
-        }
-    };
+    // var data = {
+    //     "beatles": [
+    //         {"firstName": "John", "lastName": "Lennon"},
+    //         {"firstName": "Paul", "lastName": "McCartney"},
+    //         {"firstName": "George", "lastName": "Harrison"},
+    //         {"firstName": "Ringo", "lastName": "Starr"}
+    //     ],
+    //     "name": function () {
+    //         return this.firstName + " " + this.lastName;
+    //     }
+    // };
 
-    renderView('home', default_container, data);
+    // renderView('home', default_container, data);
+    renderView('home', default_container);
 }).enter(clearPanel);
 
 Path.map("#/about").to(function () {
